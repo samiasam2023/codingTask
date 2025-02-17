11a7ea5f27b0e0f8f5fe3987f6feb5f60c0561eb
samiasam2023 <samiasam2023@gmail.com>
2025-02-16 22:45:56 -0500

    second task adding values for total sum

---
+++ src/test/java/sumOfElements.java
@@ -0,0 +1,12 @@
+public class sumOfElements {
+    public static void main(String[] args) {
+        int [] values={100, 56, 85, 45, 200};
+        int sum=0;
+        for (int add: values) {
+            sum += add;
+        }
+            System.out.println(sum);
+
+
+    }
+}
