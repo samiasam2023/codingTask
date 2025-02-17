cde07d2d02021506a784eda7818217a404894ce6
samiasam2023 <samiasam2023@gmail.com>
2025-02-16 22:17:40 -0500

    first task

---
+++ src/test/java/temperatures.java
@@ -0,0 +1,20 @@
+public class temperatures {
+    public static void main(String[] args) {
+        double [] temperature={97.0, 88.0, 66.0};
+
+        double highTemp=temperature[0];
+        double lowtemp=temperature[0];
+        for (int t=0; t<temperature.length; t++){
+            if(temperature[t]>highTemp){
+                highTemp=temperature[t];
+            }
+            if (temperature[t]<lowtemp){
+                lowtemp=temperature[t];
+            }
+        }
+        System.out.println("Highest temperature :"+highTemp);
+        System.out.println("Lowest temperature :"+lowtemp);
+
+    }
+
+}
